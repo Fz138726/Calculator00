d8188704d3a807a796305088c3e81006b22a4fe5
卓 冯 <1071723737@qq.com>
2020-02-28 04:05:20 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入数1:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("输入数2:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("输入运算符:");
+            switch (Console.ReadLine())
+            {
+                case "+": Console.WriteLine("结果：" + (n1 + n2));break;
+                case "-": Console.WriteLine("结果：" + (n1 - n2)); break;
+                case "*": Console.WriteLine("结果：" + n1 * n2); break;
+                case "/":
+                    if (n2 != 0)
+                        Console.WriteLine("结果：" + n1 / n2);
+                    else
+                        Console.WriteLine("除数不可为0");
+                    break;
+            }
+        }
+    }
+}
